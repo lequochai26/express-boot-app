b11e002ce6a5764357c79208f7c3e22ccbe31f75
Lê Quốc Hải <lequochai26@gmail.com>
2024-05-13 16:24:01 +0700

    Added ExpressBootContext class.

---
+++ system/ExpressBootContext.ts
@@ -0,0 +1,78 @@
+@ExpressBootContext.node("context")
+export default class ExpressBootContext {
+    // STATIC
+    // Static fields:
+    /**
+     * Nodes storage.
+     */
+    static nodes: { [ index: string | symbol ]: any } = {};
+
+    // Decorators:
+    /**
+     * Define a node through class with node name as given
+     * @param name - node name
+     */
+    public static node(name: string, ...args: any[]): ClassDecorator {
+        const evaluation: ClassDecorator = function (target: any) {
+            ExpressBootContext.nodes[name] = new target(...args);
+        };
+        return evaluation;
+    }
+
+    /**
+     * Define a node through config method with node name as method's name
+     * @param dependencies - dependencies will be inject into config methods
+     */
+    public static configNode(...dependencies: string[]): MethodDecorator {
+        const evaluation: MethodDecorator = function (target, propertyKey, descriptor: any) {
+            ExpressBootContext.nodes[propertyKey] = descriptor.value.call(
+                descriptor.value,
+                ...dependencies.map(
+                    dependency => ExpressBootContext.nodes[dependency]
+                )
+            );
+            
+            return {
+                ...descriptor,
+                value: function () {
+                    return ExpressBootContext.nodes[propertyKey];
+                }
+            };
+        }
+
+        return evaluation;
+    }
+
+    /**
+     * Inject dependency into method with method's name as node name.
+     * @returns New method which will return dependency on call
+     */
+    public static inject(): MethodDecorator {
+        const evaluation: MethodDecorator = function (target, propertyKey, descriptor) {
+            return <any> {
+                ...descriptor,
+                value: function () {
+                    return ExpressBootContext.nodes[propertyKey]
+                }
+            }
+        }
+        return evaluation;
+    }
+
+    // INSTANCE
+    // Constructors:
+    public constructor() {
+
+    }
+
+    // Methods:
+    public getNode(name: string): any {
+        return ExpressBootContext.nodes[name];
+    }
+
+    public getNodes(): any[] {
+        return Object.values(
+            ExpressBootContext.nodes
+        );
+    }
+}
