c9bb31df3c690bafd5a85a9be9bcf374e50eb26d
Lê Quốc Hải <lequochai26@gmail.com>
2024-05-14 12:29:10 +0700

    Added export ExpressBootCorsHandler for index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import ExpressBootContext from "./ExpressBootContext";
 import ExpressBootApp from "./ExpressBootApp";
 import ExpressBootHTTPMethod from "./ExpressBootHTTPMethod";
+import ExpressBootCorsHandler from './ExpressBootCorsHandler';
 
-export { ExpressBootContext, ExpressBootApp, ExpressBootHTTPMethod };
+export { ExpressBootContext, ExpressBootApp, ExpressBootHTTPMethod, ExpressBootCorsHandler };
