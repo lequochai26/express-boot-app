e43202717a2b8f501fa4914285e5054127462ab0
Lê Quốc Hải <lequochai26@gmail.com>
2024-05-16 03:56:53 +0700

    Modified index.ts to export this project.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
-import ExpressBootContext from "./deprecateds/ExpressBootContext";
-import ExpressBootApp from "./deprecateds/ExpressBootApp";
+import { App } from "./interfaces/App";
+import Context from "./interfaces/Context";
+import ExpressBootNode from "./interfaces/ExpressBootNode";
+import ExpressBootRequestHandler from "./interfaces/ExpressBootRequestHandler";
+import ExpressBootRequestMiddleware from "./interfaces/ExpressBootRequestMiddleware";
+import RequestDispatcher from "./interfaces/RequestDispatcher";
 import ExpressBootHTTPMethod from "./types/ExpressBootHTTPMethod";
-import ExpressBootCorsHandler from './deprecateds/ExpressBootCorsHandler';
+import ExpressBootRequestHandlerProvider from "./types/ExpressBootRequestHandlerProvider";
+import ExpressBootScript from './types/ExpressBootScript';
+import ExpressBootApp from "./ExpressBootApp";
+import ExpressBootContext from "./ExpressBootContext";
+import ExpressBootRequestDispatcher from "./ExpressBootRequestDispatcher";
 
-export { ExpressBootContext, ExpressBootApp, ExpressBootHTTPMethod, ExpressBootCorsHandler };
+export { App, Context, ExpressBootNode, ExpressBootRequestHandler, ExpressBootRequestMiddleware, RequestDispatcher, ExpressBootHTTPMethod, ExpressBootRequestHandlerProvider, ExpressBootScript, ExpressBootApp, ExpressBootContext, ExpressBootRequestDispatcher };
