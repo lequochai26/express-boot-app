64ed976af58af7b7d66275354246d295bf6bfbd7
Lê Quốc Hải <lequochai26@gmail.com>
2024-05-16 02:15:45 +0700

    Added JSDoc to Context interface's methods.

--- src/interfaces/Context.ts
+++ src/interfaces/Context.ts
@@ -5,12 +5,52 @@ import ExpressBootRequestHandler from "./ExpressBootRequestHandler";
 import ExpressBootRequestMiddleware from "./ExpressBootRequestMiddleware";
 
 export default interface Context {
+    /**
+     * Get a specific node in 
+     * @param name Node name
+     * @returns Node
+     */
     getNode(name: string): any;
+
+    /**
+     * Get all nodes in system
+     * @returns All nodes in system
+     */
     getNodes(): ExpressBootNode[];
+
+    /**
+     * Get all request handlers in system
+     * @returns All request handlers in system
+     */
     getRequestHandlers(): ExpressBootRequestHandler[];
+
+    /**
+     * Get all request middlewares is system
+     * @returns All request middlewares in system
+     */
     getRequestMiddlewares(): ExpressBootRequestMiddleware[];
+
+    /**
+     * Get CORS configurer
+     * @returns CORS configurer if available
+     */
     getCorsConfigurer(): ExpressBootRequestHandlerProvider;
+
+    /**
+     * Get Multer configurer
+     * @returns Multer configurer if available
+     */
     getMulterConfigurer(): ExpressBootRequestHandlerProvider;
+
+    /**
+     * Get all scripts in system
+     * @returns All scripts in system
+     */
     getScripts(): ExpressBootScript[];
+
+    /**
+     * Load context
+     * @param path Sources path
+     */
     load(path: string): Promise<void>;
 }
