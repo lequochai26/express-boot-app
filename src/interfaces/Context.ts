73467e9653f06af0ff16e5f2cd1c1f6513b6d623
Lê Quốc Hải <lequochai26@gmail.com>
2024-05-15 13:35:03 +0700

    Added ExpressBootNode interface and make use it.

--- src/interfaces/Context.ts
+++ src/interfaces/Context.ts
@@ -1,11 +1,12 @@
 import ExpressBootRequestHandlerProvider from "../types/ExpressBootRequestHandlerProvider";
 import ExpressBootScript from "../types/ExpressBootScript";
+import ExpressBootNode from "./ExpressBootNode";
 import ExpressBootRequestHandler from "./ExpressBootRequestHandler";
 import ExpressBootRequestMiddleware from "./ExpressBootRequestMiddleware";
 
 export default interface Context {
     getNode(name: string): any;
-    getNodes(): any[];
+    getNodes(): ExpressBootNode[];
     getRequestHandlers(): ExpressBootRequestHandler[];
     getRequestMiddlewares(): ExpressBootRequestMiddleware[];
     getCorsConfigurer(): ExpressBootRequestHandlerProvider;
