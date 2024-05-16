9acaf7b0a4fdde50082082171b295621086ab4e2
Lê Quốc Hải <lequochai26@gmail.com>
2024-05-17 03:59:54 +0700

    Added JSDoc for ExpressBootRequestHandler.

--- src/interfaces/ExpressBootRequestHandler.ts
+++ src/interfaces/ExpressBootRequestHandler.ts
@@ -1,8 +1,22 @@
 import { RequestHandler } from "express";
 import ExpressBootHTTPMethod from "../types/ExpressBootHTTPMethod";
 
+/**
+ * Represent a request handler in ExpressBoot system
+ */
 export default interface ExpressBootRequestHandler {
+    /**
+     * Request mapping path
+     */
     path: string;
+
+    /**
+     * HTTP method
+     */
     method: ExpressBootHTTPMethod;
+
+    /**
+     * Request handler function
+     */
     handler: RequestHandler;
 }
