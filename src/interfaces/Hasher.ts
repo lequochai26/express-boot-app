a5b8ad702c1a8b37abb7701b81db30f50498451c
Lê Quốc Hải <lequochai26@gmail.com>
2024-05-17 04:01:49 +0700

    Added JSDoc for Hasher interface.

--- src/interfaces/Hasher.ts
+++ src/interfaces/Hasher.ts
@@ -1,3 +1,11 @@
+/**
+ * Text hasher
+ */
 export default interface Hasher {
+    /**
+     * Hash a text with specific hashing algorithm
+     * @param content Content to be hash
+     * @param algorithm Hashing algorithm
+     */
     hash(content: string, algorithm: HashAlgorithm): string;
 }
