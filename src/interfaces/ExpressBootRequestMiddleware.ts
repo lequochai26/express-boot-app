fabd521f5b0ed7f4449ab45ce394eae826caf10b
Lê Quốc Hải <lequochai26@gmail.com>
2024-05-17 04:00:48 +0700

    Added JSDoc for ExpressBootRequestMiddleware.

--- src/interfaces/ExpressBootRequestMiddleware.ts
+++ src/interfaces/ExpressBootRequestMiddleware.ts
@@ -1,6 +1,16 @@
 import { RequestHandler } from "express";
 
+/**
+ * Represent a request middleware in ExpressBoot system
+ */
 export default interface ExpressBootRequestMiddleware {
+    /**
+     * Request mapping path
+     */
     path: string;
+
+    /**
+     * Request middleware function
+     */
     middleware: RequestHandler;
 }
