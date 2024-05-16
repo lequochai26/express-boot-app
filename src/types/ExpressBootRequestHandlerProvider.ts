e5a33b503a58b23d38afe0a2dceb35d5de1a9f8c
Lê Quốc Hải <lequochai26@gmail.com>
2024-05-17 04:13:19 +0700

    Added JSDoc for ExpressBootRequestHandlerProvider type.

--- src/types/ExpressBootRequestHandlerProvider.ts
+++ src/types/ExpressBootRequestHandlerProvider.ts
@@ -1,4 +1,7 @@
 import { RequestHandler } from "express";
 
+/**
+ * Express's RequestHandler provider (often use to represent for a configurer)
+ */
 type ExpressBootRequestHandlerProvider = () => RequestHandler;
 export default ExpressBootRequestHandlerProvider;
