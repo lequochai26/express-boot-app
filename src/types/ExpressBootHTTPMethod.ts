2b632479acc60bbf2cbdfd21ba119f42136bec51
Lê Quốc Hải <lequochai26@gmail.com>
2024-05-17 04:11:58 +0700

    Added JSDoc for ExpressBootHTTPMethod type.

--- src/types/ExpressBootHTTPMethod.ts
+++ src/types/ExpressBootHTTPMethod.ts
@@ -1,2 +1,5 @@
+/**
+ * Available HTTP methods in ExpressBoot system
+ */
 type ExpressBootHTTPMethod = "GET" | "POST" | "PUT" | "DELETE" | "PATCH" | "HEAD" | "OPTIONS";
 export default ExpressBootHTTPMethod;
