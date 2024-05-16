0c1d88732b9de00adf116c279c0efaafb87c6980
Lê Quốc Hải <lequochai26@gmail.com>
2024-05-17 03:56:39 +0700

    Added JSDoc for App interface.

--- src/interfaces/App.ts
+++ src/interfaces/App.ts
@@ -1,8 +1,24 @@
 import { Express } from "express";
 import http from 'http';
 
+/**
+ * Represent the ExpressBoot's Application
+ */
 export interface App {
+    /**
+     * Start ExpressBoot app
+     * @returns app, server
+     */
     start(): Promise<{ app: Express, server: http.Server }>;
+
+    /**
+     * Check if this ExpressBoot app started
+     * @returns started
+     */
     started(): boolean;
+
+    /**
+     * Stop ExpressBoot app
+     */
     stop(): Promise<void>;
 }
