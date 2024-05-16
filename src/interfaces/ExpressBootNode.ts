8ece7e893b8060f7d1a5f2ff62f8f35c3759e2f1
Lê Quốc Hải <lequochai26@gmail.com>
2024-05-17 03:58:55 +0700

    Added JSDoc for ExpressBootNode interface's properties.

--- src/interfaces/ExpressBootNode.ts
+++ src/interfaces/ExpressBootNode.ts
@@ -2,6 +2,13 @@
  * Represent a node in ExpressBoot system
  */
 export default interface ExpressBootNode {
+    /**
+     * Node name
+     */
     name: string | symbol;
+
+    /**
+     * Node object
+     */
     value: any;
 }
