0394163b233412af5449b4b157a510a8a390ca14
Lê Quốc Hải <lequochai26@gmail.com>
2024-05-17 04:08:39 +0700

    Added JSDoc for RequestDispatcher interface.

--- src/interfaces/RequestDispatcher.ts
+++ src/interfaces/RequestDispatcher.ts
@@ -1,6 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 import ExpressBootHTTPMethod from "../types/ExpressBootHTTPMethod";
 
+/**
+ * Request dispatcher
+ */
 export default interface RequestDispatcher {
+    /**
+     * Dispatching the request handling execution from a request handler to another.
+     * @param request Request
+     * @param response Response
+     * @param next Next function
+     * @param to Destination request handler's path
+     * @param method HTTP method
+     */
     dispatch(request: Request, response: Response, next: NextFunction, to: string, method: ExpressBootHTTPMethod): Promise<void>;
 }
