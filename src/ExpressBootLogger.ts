12848d93c4aa8678ee27fa3dd6e6244ff0a762ee
Lê Quốc Hải <lequochai26@gmail.com>
2024-05-17 22:21:26 +0700

    Added ExpressBootLogger implementation of Logger interface.

---
+++ src/ExpressBootLogger.ts
@@ -0,0 +1,43 @@
+import { bgCyan, bgRed, bgYellow } from "colors";
+import ExpressBootContext from "./ExpressBootContext";
+import Logger from "./interfaces/Logger";
+
+@ExpressBootContext.node("logger")
+export default class ExpressBootLogger implements Logger {
+    // Constructor:
+    public constructor() {
+
+    }
+
+    // Private methods:
+    private log(content: string, type: "INFO" | "WARNING" | "ERROR"): void {
+        content = `${new Date().toLocaleTimeString("vi").padEnd(10)}${type.charAt(0).padEnd(3)}${content}`
+
+        content = (
+            type === 'INFO'
+            ?
+            bgCyan(content)
+            :
+            type === 'WARNING'
+            ?
+            bgYellow(content)
+            :
+            bgRed(content)
+        );
+
+        console.log(content);
+    }
+
+    // Methods:
+    public info(content: string): void {
+        this.log(content, "INFO");
+    }
+
+    public warning(content: string): void {
+        this.log(content, "WARNING");
+    }
+
+    public error(content: string): void {
+        this.log(content, "ERROR");
+    }
+}
